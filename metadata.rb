1170af8325517588828e74441dc7cef35f3ee5be
Jesse Nelson <spheromak@gmail.com>
2012-11-15 16:14:16 +0900

    bump version, fixes some naming inconsistancies

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email "spheromak@gmail.com"
 license          "APL2"
 description      "Helper Methods for Databags"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.0.3"
 
