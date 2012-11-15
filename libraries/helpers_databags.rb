e538ccebafbbb0bedf98a5acd8e159026e75d48f
Jesse Nelson <spheromak@gmail.com>
2012-11-15 16:45:33 +0900

    move to singleton

--- libraries/helpers_databags.rb
+++ libraries/helpers_databags.rb
@@ -1,21 +1,22 @@
         
 module Helpers
-    module DataBags
+  module DataBags
+    class << self
+      # data_bag_fqdn
+      #  This helper converts a fqdn from . notation to -'s 
+      # used to encode hostnames into databag names. since databags restrict using periods
+      def data_bag_fqdn(fqdn=node.fqdn)
+        escape_bagname(fqdn)
+      end
 
-    # data_bag_fqdn
-    #  This helper converts a fqdn from . notation to -'s 
-    # used to encode hostnames into databag names. since databags restrict using periods
-    def data_bag_fqdn(fqdn=node.fqdn)
-      escape_bagname(fqdn)
+      # escape  . -> - and / -> _  
+      # so 1.1/3 -> 1-1_3 
+      def escape_bagname(name)
+        n = name.gsub(/\./,'-')
+        n.gsub(/\//,'_')
+      end
+
     end
-
-    # escape  . -> - and / -> _  
-    # so 1.1/3 -> 1-1_3 
-    def escape_bagname(name)
-      n = name.gsub(/\./,'-')
-      n.gsub(/\//,'_')
-    end
-
   end
 end
 
